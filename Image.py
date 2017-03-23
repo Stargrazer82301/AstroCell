c2c7aeec16f6cba9c304e2d7edcdaa175da221f7
Chris Clark <cjrc88@gmail.com>
2017-03-23 12:44:40 +0000

    Created RGB object for bundling all three bands' Image objects together

--- Image.py
+++ Image.py
@@ -25,8 +25,78 @@ plt.ioff()
 
 
 
+class RGB():
+    """ Class that stores an RGB triplet of image arrays; the methods of an RGB object concern handling all three bands together"""
+
+
+
+    def __init__(self, in_path):
+        """ Initialise the RGB object """
+
+        # Read in the image file, and convert to array
+        bitmap_image = PIL.Image.open(in_path)
+        rgb_image = np.array(bitmap_image)
+
+        # Store the full rgb cube
+        self.cube = rgb_image
+
+        # Initialise AstroCell.Image object for each channel
+        self.r = Image(rgb_image[:,:,0])
+        self.g = Image(rgb_image[:,:,1])
+        self.b = Image(rgb_image[:,:,2])
+
+
+
+    def Iter(self):
+        """A method that yields a tuple containing each channel, for iterating over"""
+
+        # Create and return tuple of image objects
+        return (self.r,self.g,self.b)
+
+
+
+    def BlackOnWhite(self):
+        """ A method that determines whether an image is on a black background; if not, image is inverted so that it is """
+
+        # Iterate over rgb, performing rough canny extration
+        cube_canny = np.zeros(self.cube.shape)
+        for band in self.Iter():
+            band.CannyCells()
+
+
+#        # Get pixels values of image, with black (0) and white (255) pixels removed
+#        in_values = self.cube.copy().flatten()
+#        in_values = in_values[np.where(in_values!=0)]
+#        in_values = in_values[np.where(in_values!=255)]
+#        in_values = in_values[np.where(np.isnan(in_values)==False)]
+#
+#        r_canny = skimage.feature.canny(self.r.map, sigma=1.0)
+#        r_canny_label = skimage.measure.label(np.invert(r_canny), connectivity=1)
+#        astropy.io.fits.writeto('/home/chris/r_canny_label.fits', r_canny_label.astype(float), clobber=True)
+#
+#
+#
+#
+#        # Get distribution of pixel values, and which values are over/under median
+#        in_clip = SigmaClip(in_values, median=True, tolerance=0.0001, sigma_thresh=0.5)
+#
+#        # Construct histogram to find peak of pixel value distribution (bin size 10x Freedman-Diaconis bin size)
+#        hist_bin_width = 10.0 * 2.0 * scipy.stats.iqr(in_values) * in_values.size**(-1./3.)
+#        hist_bins = int( np.round( (np.nanmax(in_values)-np.nanmin(in_values)) / hist_bin_width ) )
+#        pdb.set_trace()
+#        hist = np.histogram(in_values, bins=hist_bins)
+#
+#
+#        in_over = in_values[np.where(in_values>in_clip[1])]
+#        in_under = in_values[np.where(in_values<in_clip[1])]
+#        pdb.set_trace()
+
+
+
+
+
 class Image():
-    """ Class that stores an image array, and provides various methods to process that image in some way;the output of these methods will be a new
+    """ Class that stores an image array, and provides various methods to process that image in some way; the output of these methods can be a new
     attribute of the Image object in question, each of which will *itslf* be a new Image object, with the same processing functionality """
 
 
